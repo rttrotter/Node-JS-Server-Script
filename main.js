96ce21ec332d30e84efcb2770370e1846d295306
rttrotter <58933487+rttrotter@users.noreply.github.com>
2021-05-18 14:22:11 -0500

    Update main.js
    Code compiles, at least have an HTTP server running. Now to figure out a way to test it, and make it do what I want.

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ Application: server REST endpoint
 //includes
 var https = require('https'); //create https object
 //variables
-var reverse = ""; 
+let reverse = ''; // blank variable to hold the reverse of the message 
 
-
+/*
 //setting up server settings
 const options = 
 {
-	hostename: 127.0.0.1,//what the server will be listening for
+	hostname: 127.0.0.1,//what the server will be listening for
 	port: 9001,//port we're listening on
 	path: './testing',//folder for related items?
 	method: 'POST',//using post for deciphering requests
@@ -22,21 +22,22 @@ const options =
 		'Content-Length': data.length
 	}
 }
-
+*/
 //create the server
-http.createServer(function (inbound, outbound) 
+https.createServer(function (inbound, outbound) 
 {
 	var msgBody = ""; //create a blank message body
 	//when recieving data, append it to the msgBody
 	//listening to data events 
-	inbound.on('data', function (chunk)
+	inbound.on('data', function()
 	{
+		
 		msgBody += chunk;
 		
 	});
 	//listening for end events
 	inbound.on('end', function()
-	(
+	{
 		console.log('POST RAW JSON: '+ msgBody); // testing to print out output
 		var jHolder = JSON.stringify(msgBody);
 		//create for loop to flip the script
@@ -50,7 +51,9 @@ http.createServer(function (inbound, outbound)
 		outbound.writeHead(200, {'Content-Tyle': 'application/json' });
 		//sends the information out
 		//write the data to the user by making it a json
-		outbound.end(reverse);
-	}
+		outbound.end(JSON.stringify({'data': reverse}));
+	});
 
 }).listen(9001); // server will listen on port 9001
+
+console.log('server running on the port 9001');
